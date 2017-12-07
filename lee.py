29ef10baa485776ed7f65ccb0ebf7a0850972bd5
Abhishek Singh <asraisingh@gmail.com>
2017-12-07 21:54:08 +0530

    Started saving results in xls file. Saving only those profile details which contains email id in there description. Email id is also saving in the separate column. :

--- lee.py
+++ lee.py
@@ -1,33 +1,49 @@
 #!/usr/bin/env python3
 
-import datetime as dt
-import json, sys
-
-from googleapiclient.discovery import build
-import pprint
+from googleapiclient.discovery import build                     #For using Google Custom Search Engine API
+import datetime as dt                                           #Importing system date for the naming of the output file.
+import sys                                                      
+from xlwt import Workbook                                       #For working on xls file.
+import re                                                       #For email search using regex.
 
 if __name__ == '__main__':
-    # Create an output file name in the format "srch_res_yyyyMMdd_hhmmss.json"
+    # Create an output file name in the format "srch_res_yyyyMMdd_hhmmss.xls in output folder"
     now_sfx = dt.datetime.now().strftime('%Y%m%d_%H%M%S')
     output_dir = './output/'
-    output_fname = output_dir + 'srch_res_' + now_sfx + '.json'
+    output_fname = output_dir + 'srch_res_' + now_sfx + '.xls'
     search_term = sys.argv[1]
     num_requests = int(sys.argv[2])
     
-    # Key codes we created earlier for the Google CustomSearch API
-    my_api_key = "replace_with_you_api_key"  #Read readme.md to know how to get you api key.
-    my_cse_id = "011658049436509675749:gkuaxghjf5u"
-
-    output_f = open(output_fname, 'a+')
+    my_api_key = "replace_with_you_api_key"                 #Read readme.md to know how to get you api key.
+    my_cse_id = "011658049436509675749:gkuaxghjf5u"         #Google CSE which searches possible LinkedIn profile according to query.
 
     service = build("customsearch", "v1", developerKey=my_api_key)
+
+    wb=Workbook()
+    sheet1 = wb.add_sheet(search_term)                      #Excel sheet created with name of the search term.
+    wb.save(output_fname)
+    sheet1.write(0,0,'Name')
+    sheet1.write(0,1,'Profile Link')
+    sheet1.write(0,2,'Snippet')
+    sheet1.write(0,3,'Present Organisation')
+    sheet1.write(0,4,'Location')
+    sheet1.write(0,5,'Role')
+    sheet1.write(0,6,'Email')
+    sheet1.col(0).width = 256 * 20
+    sheet1.col(1).width = 256 * 50
+    sheet1.col(2).width = 256 * 100
+    sheet1.col(3).width = 256 * 20
+    sheet1.col(4).width = 256 * 20
+    sheet1.col(5).width = 256 * 50
+    sheet1.col(6).width = 256 * 50
+    wb.save(output_fname)
+
+    row = 1                                                 #To insert the data in the next row.
 
     #Function to perform google search.
     def google_search(search_term, cse_id, start_val, **kwargs):
         res = service.cse().list(q=search_term, cx=cse_id, start=start_val, **kwargs).execute()
-        output = json.dumps(res, sort_keys=True, indent=2)
-        output_f.write(output)
-        print('Wrote 10 search results...')
+        return res
 
     for i in range(0, num_requests):
         # This is the offset from the beginning to start getting the results from
@@ -36,8 +52,30 @@ if __name__ == '__main__':
         results = google_search(search_term,
             my_cse_id,
             start_val,
-            num=10
+            num=10                                          #num value can be 1 to 10. It will give the no. of results. 
         )
- 
-    output_f.close()
+
+        for profile in range (0, 10):
+            title = results['items'][profile]['title']
+            link = results['items'][profile]['link']
+            snippet = results['items'][profile]['snippet']
+            org = results['items'][profile]['pagemap']['person'][0]['org']
+            location = results['items'][profile]['pagemap']['person'][0]['location']
+            role = results['items'][profile]['pagemap']['person'][0]['role']
+            myList = [item for item in snippet.split('\n')]
+            newSnippet = ' '.join(myList)
+            contain = re.search(r'[\w\.-]+@[\w\.-]+', newSnippet)
+            if contain is not None:
+                print(title[:-23])
+                sheet1.write(row,0,title[:-23])
+                sheet1.write(row,1,link)
+                sheet1.write(row,2,newSnippet)
+                sheet1.write(row,3,org)
+                sheet1.write(row,4,location)
+                sheet1.write(row,5,role)
+                sheet1.write(row,6,contain[0])
+                print('Wrote {} search result(s)...'.format(row))
+                row = row + 1
+
+    wb.save(output_fname)
     print('Output file "{}" written.'.format(output_fname)) 
